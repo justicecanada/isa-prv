869d92e7fb9f2a57ac41bcc5b5e1670ddfb18930
Jason Pesant <jason.pesant@justice.gc.ca>
2023-09-19 10:53:13 -0400

    Basic WET Implementation

--- Interview/Interview.UI/Controllers/EmailsController.cs
+++ Interview/Interview.UI/Controllers/EmailsController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Interview.UI.Controllers
 {
-    public class EmailsController : Controller
+    public class EmailsController : GoC.WebTemplate.CoreMVC.Controllers.WebTemplateBaseController
     {
 
         #region Declarations
@@ -18,7 +18,8 @@ namespace Interview.UI.Controllers
 
         #region Constructors
 
-        public EmailsController(DalSql dal, IMapper mapper)
+        public EmailsController(DalSql dal, IMapper mapper, GoC.WebTemplate.Components.Core.Services.IModelAccessor modelAccessor)
+            : base(modelAccessor)
         {
             _dal = dal;
             _mapper = mapper;
--- Interview/Interview.UI/Startup.cs
+++ Interview/Interview.UI/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Localization;
 using Newtonsoft.Json;
 using System.Data.SqlTypes;
 using System.Globalization;
+using GoC.WebTemplate.Components.Core.Services;
 
 namespace Interview.UI
 {
@@ -46,6 +47,9 @@ namespace Interview.UI
                 options.Filters.Add(typeof(AssemblyVersionFilter));
             });
 
+            // WET
+            services.AddModelAccessor();
+            services.ConfigureGoCTemplateRequestLocalization(); // >= v2.3.0
         }
 
         private void ConfigureLocalizationServices(IServiceCollection services, IMvcBuilder builder)
@@ -96,6 +100,10 @@ namespace Interview.UI
                     name: "default",
                     pattern: "{controller=Default}/{action=Index}");
             });
+
+            // WET
+            app.UseRequestLocalization(); // >= v2.3.0
+            app.UseRequestLocalization(CultureConfiguration.GetLocalizationOptions()); // <= v2.3.0
 
         }
 
