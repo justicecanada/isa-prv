8e91301b599d77727bd262b8fb320180a6dff4fe
Stewart Partington <Stewart.Partington@justice.gc.ca>
2023-09-26 08:48:31 -0400

    Add Default client side resources.

--- Interview/Interview.UI/wwwroot/js/Resources.fr.js
+++ Interview/Interview.UI/wwwroot/js/Resources.fr.js
@@ -4,6 +4,14 @@
         ArriveCandidat: "Arrivé du candidat",
         ArriveComite: "Arrivé des membres du comité",
         EvaluationComite: "Évaluation du comité"
+    },
+
+    Calendar: {
+        Today: "Today (fr)",
+        Month: "Month (fr)",
+        Week: "Week (fr)",
+        Day: "Day (fr)",
+        List: "List (fr)"
     }
 
 }
