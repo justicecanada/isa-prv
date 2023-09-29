284c4945df6592bf4c5fe3aa78b974d8a8fe8604
Stewart Partington <Stewart.Partington@justice.gc.ca>
2023-09-29 12:09:15 -0400

    Interim checkin for registering client side resources.

--- Interview/Interview.UI/Controllers/BaseController.cs
+++ Interview/Interview.UI/Controllers/BaseController.cs
@@ -9,27 +9,52 @@ namespace Interview.UI.Controllers
     [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
     public class BaseController : WebTemplateBaseController
     {
-        
+
+        #region Declarations
+
+        private string _assemblyVersion;
+
+        #endregion
+
+        #region Constructors
+
         public BaseController(IModelAccessor modelAccessor) : base(modelAccessor)
         {
 
             //https://github.com/wet-boew/cdts-DotNetTemplates/blob/master/samples/dotnet-coremvc-sample/Controllers/GoCWebTemplateSamplesController.cs
 
-            var assemblyVersion = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
- 
             // css
-            WebTemplateModel.HTMLHeaderElements.Add($"<link rel=\"stylesheet\" href=\"/css/site.css?v={assemblyVersion}\" />");
+            WebTemplateModel.HTMLHeaderElements.Add($"<link rel=\"stylesheet\" href=\"/css/site.css?v={AssemblyVersion}\" />");
             //WebTemplateModel.HTMLHeaderElements.Add("<link href=\"/lib/jquery-ui-1.13.2.custom/jquery-ui.min.css\" rel=\"stylesheet\" />");
             WebTemplateModel.HTMLHeaderElements.Add("<script src=\"/lib/jquery/dist/jquery.min.js\"></script>");
 
             // js
-            WebTemplateModel.HTMLBodyElements.Add($"<script src=\"/js/site.js?v={assemblyVersion} defer \"></script>");
+            WebTemplateModel.HTMLBodyElements.Add($"<script src=\"/js/site.js?v={AssemblyVersion} defer \"></script>");
             //WebTemplateModel.HTMLBodyElements.Add($"<script src=\"/lib/jquery-ui-1.13.2.custom/jquery-ui.min.js\"></script>");
 
             // Identifier
-            WebTemplateModel.VersionIdentifier = assemblyVersion;
+            WebTemplateModel.VersionIdentifier = AssemblyVersion;
 
         }
+
+        #endregion
+
+        #region Properties
+
+        protected string AssemblyVersion
+        {
+            get
+            {
+
+                if (string.IsNullOrEmpty(_assemblyVersion))
+                    _assemblyVersion = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+
+                return _assemblyVersion;
+
+            }
+        }
+
+        #endregion
 
     }
 
