37f529b591136150d5ddaeea88b4d53797fb38fc
Stewart Partington <Stewart.Partington@justice.gc.ca>
2023-09-29 10:47:23 -0400

    Add MockUser migration and delete MockIdentityContext

--- Interview/Interview.UI/Migrations/20230929144317_AddMockUsers.Designer.cs
+++ Interview/Interview.UI/Migrations/20230929144317_AddMockUsers.Designer.cs
@@ -4,6 +4,7 @@ using Interview.UI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Interview.UI.Migrations
 {
     [DbContext(typeof(SqlContext))]
-    partial class SqlContextModelSnapshot : ModelSnapshot
+    [Migration("20230929144317_AddMockUsers")]
+    partial class AddMockUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
