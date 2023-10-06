24f16a59c9c7b82285e02ac046a8d95f57abec92
Pierre Collette <Pierre.Collette@justice.gc.ca>
2023-10-06 14:27:08 -0300

    Emails changes

---
+++ Interview/Interview.UI/Models/VmEmails.cs
@@ -0,0 +1,11 @@
+﻿using Interview.Entities;
+using Interview.UI.Models.CustomValidation;
+using System.ComponentModel.DataAnnotations;
+
+namespace Interview.UI.Models
+{
+    public class VmEmails : VmBase
+    {
+     
+    }
+}
