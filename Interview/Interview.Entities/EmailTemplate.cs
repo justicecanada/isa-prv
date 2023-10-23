a2394ec5cc559d932f26e1791e736c0429a12e9d
Pierre Collette <Pierre.Collette@justice.gc.ca>
2023-10-23 14:44:19 -0300

    Updated Code

--- Interview/Interview.Entities/EmailTemplate.cs
+++ Interview/Interview.Entities/EmailTemplate.cs
@@ -9,25 +9,21 @@ namespace Interview.Entities
     
     public class EmailTemplate : EntityBase
     {
-        /* 
-         * 
-         * Need to FIX this
-         * 
-         * 
+         
+        
         public EmailTemplate()
         {
 			EmailTemplates = new List<EmailTemplate>();
-
-        }*/
+        }
 
 
 		public Guid ContestId { get; set; }
         public EmailTypes EmailType { get; set; }
-        //public List<EmailTemplate> EmailTemplates { get; set; }
+        public List<EmailTemplate> EmailTemplates { get; set; }
+
         public string? EmailSubject { get; set; }
         public string? EmailBody { get; set; }
         public string? EmailCC { get; set; }
-
-    }
+	}
 
 }
--- Interview/Interview.UI/Models/VmEmails.cs
+++ Interview/Interview.UI/Models/VmEmails.cs
@@ -9,28 +9,16 @@ namespace Interview.UI.Models
     public class VmEmails : VmBase
     {
         [Required]
-        [Display(Name = "EmailSubjectEN")]
-        [MaxLength(128, ErrorMessage = "EmailSubjectENMaxLength")]
-		public string? EmailSubjectEN { get; set; }
+        [Display(Name = "EmailSubject")]
+        [MaxLength(128, ErrorMessage = "EmailSubjectMaxLength")]
+		public string? EmailSubject { get; set; }
 
-        [Display(Name = "CCEN")]
-        public string? CCEN { get; set; }
+        [Display(Name = "EmailCC")]
+        public string? EmailCC { get; set; }
 
         [Required]
-        [Display(Name = "DescriptionEN")]
-        public string? DescriptionEN { get; set; }
-
-		[Required]
-		[Display(Name = "EmailSubjectFR")]
-		[MaxLength(128, ErrorMessage = "EmailSubjectFRMaxLength")]
-		public string? EmailSubjectFR { get; set; }
-
-		[Display(Name = "CCFR")]
-		public string? CCFR { get; set; }
-
-		[Required]
-		[Display(Name = "DescriptionFR")]
-		public string? DescriptionFR { get; set; }
+        [Display(Name = "EmailBody")]
+        public string? EmailBody { get; set; }
 
 	}
 }
