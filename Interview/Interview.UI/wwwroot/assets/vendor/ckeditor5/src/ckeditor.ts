a79c003cdaaa6b41967f71f8fca8b0af0129b56d
Stewart Partington <Stewart.Partington@justice.gc.ca>
2024-01-03 16:54:43 -0500

    Readding CkEditor

---
+++ Interview/Interview.UI/wwwroot/assets/vendor/ckeditor5/src/ckeditor.ts
@@ -0,0 +1,47 @@
+/**
+ * @license Copyright (c) 2014-2024, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+import { InlineEditor } from '@ckeditor/ckeditor5-editor-inline';
+
+import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
+import type { EditorConfig } from '@ckeditor/ckeditor5-core';
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { Link } from '@ckeditor/ckeditor5-link';
+import { List } from '@ckeditor/ckeditor5-list';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { Undo } from '@ckeditor/ckeditor5-undo';
+
+// You can read more about extending the build with additional plugins in the "Installing plugins" guide.
+// See https://ckeditor.com/docs/ckeditor5/latest/installation/plugins/installing-plugins.html for details.
+
+class Editor extends InlineEditor {
+	public static override builtinPlugins = [
+		Bold,
+		Essentials,
+		Italic,
+		Link,
+		List,
+		Paragraph,
+		Undo
+	];
+
+	public static override defaultConfig: EditorConfig = {
+		toolbar: {
+			items: [
+				'bold',
+				'italic',
+				'link',
+				'bulletedList',
+				'numberedList',
+				'|',
+				'undo',
+				'redo'
+			]
+		},
+		language: 'en'
+	};
+}
+
+export default Editor;
