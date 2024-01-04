ff05d0fe7d8550ba131d7e06c5b8dc6f412ecb4f
Stewart Partington <Stewart.Partington@justice.gc.ca>
2024-01-04 11:47:30 -0500

    Interim checkin

--- Interview/Interview.UI/Models/VmEmailTemplate.cs
+++ Interview/Interview.UI/Models/VmEmailTemplate.cs
@@ -1,4 +1,5 @@
 ﻿using Interview.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace Interview.UI.Models
 {
@@ -8,12 +9,19 @@ namespace Interview.UI.Models
 
         public Guid ContestId { get; set; }
 
+        [Required]
+        [Display(Name = "EmailType")]
         public EmailTypes EmailType { get; set; }
 
+        [Required]
+        [Display(Name = "EmailSubject")]
         public string? EmailSubject { get; set; }
 
+        [Required]
+        [Display(Name = "EmailBody")]
         public string? EmailBody { get; set; }
 
+        [Display(Name = "EmailCC")]
         public string? EmailCC { get; set; }
 
     }
