028d2f031c2bfeb4ce28289cb1c2a4ecaf67b9b9
Stewart Partington <Stewart.Partington@justice.gc.ca>
2024-01-15 11:06:18 -0500

    Interim checkin

--- Interview/Interview.UI/Models/Default/VmPrivacyStatementModal.cs
+++ Interview/Interview.UI/Models/Default/VmPrivacyStatementModal.cs
@@ -1,10 +1,13 @@
-﻿namespace Interview.UI.Models.Default
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Interview.UI.Models.Default
 {
     
     public class VmPrivacyStatementModal
     {
 
-
+        [Display(Name = "HasAcceptedPrivacyStatement")]
+        public bool HasAcceptedPrivacyStatement { get; set; }
 
     }
 
--- Interview/Interview.UI/wwwroot/js/Default/PrivacyStatementModal.js
+++ Interview/Interview.UI/wwwroot/js/Default/PrivacyStatementModal.js
@@ -8,7 +8,6 @@
 
     Init: function () {
 
-        debugger;
         this.HookupMagnificPopup();
 
         if (showPrivacyStatementModal) {
@@ -24,6 +23,19 @@
                 }
             });
         }
+
+    },
+
+    HookupModalHandlers: function () {
+
+        this.Form = $("#privForm")[0];
+
+        $(this.SubmitButtonSelector).on("click", priv.Post);
+
+        $(this.CancelButtonSelector).on("click", function (e) {
+            e.preventDefault();
+            $.magnificPopup.instance.close();
+        });
 
     },
 
@@ -45,7 +57,7 @@
         $.get(priv.Uri)
             .done(function (data, textStatus, jqXHR) {
                 $(priv.ModalSelector).html($(data));
-                //priv.HookupModalHandlers();
+                priv.HookupModalHandlers();
             })
             .fail(function (data, textStatus, jqXHR) {
                 $.magnificPopup.close();
@@ -55,6 +67,39 @@
 
     Close: function () {
         $(priv.ModalSelector).empty();
+    },
+
+    Post: function (e) {
+
+        e.preventDefault();
+        var formData = $(priv.Form).serialize();
+
+        $.ajax({
+            type: "POST",
+            url: priv.Uri,
+            data: formData,
+            success: priv.PostSuccess,
+            fail: priv.PostFail
+        });
+
+    },
+
+    PostSuccess: function (data) {
+
+        if (data.result) {
+            $.magnificPopup.close();
+            //table.UpdateTable(data.item);
+            //interview.HookupMagnificPopup();
+        }
+        else {
+            $(priv.ModalSelector).html($(data));
+            priv.HookupModalHandlers();
+        }
+
+    },
+
+    PostFail: function (e) {
+        $.magnificPopup.close();
     },
 
 }
