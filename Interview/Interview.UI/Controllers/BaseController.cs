29b2fa9c01c3ae2ac23e96be0fef71135f19cf93
Stewart Partington <Stewart.Partington@justice.gc.ca>
2024-01-24 12:46:47 -0500

    Remove unused tags.

--- Interview/Interview.UI/Controllers/BaseController.cs
+++ Interview/Interview.UI/Controllers/BaseController.cs
@@ -47,12 +47,10 @@ namespace Interview.UI.Controllers
 
             // css
             WebTemplateModel.HTMLHeaderElements.Add($"<link rel=\"stylesheet\" href=\"/css/site.css?v={BuildId}\" />");
-            //WebTemplateModel.HTMLHeaderElements.Add("<link href=\"/lib/jquery-ui-1.13.2.custom/jquery-ui.min.css\" rel=\"stylesheet\" />");
             WebTemplateModel.HTMLHeaderElements.Add("<script src=\"/lib/jquery/dist/jquery.min.js\"></script>");
 
             // js
             WebTemplateModel.HTMLBodyElements.Add($"<script src=\"/js/site.js?v={BuildId} defer \"></script>");
-            //WebTemplateModel.HTMLBodyElements.Add($"<script src=\"/lib/jquery-ui-1.13.2.custom/jquery-ui.min.js\"></script>");
 
             // Identifier
             WebTemplateModel.VersionIdentifier = AssemblyVersion;
