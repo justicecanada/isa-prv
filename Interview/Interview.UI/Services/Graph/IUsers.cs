578d6cd60d0b70c7f2426201d7157e4211084bbd
Stewart Partington <Stewart.Partington@justice.gc.ca>
2024-01-25 15:21:05 -0500

    Refactoring

--- Interview/Interview.UI/Services/Graph/IUsers.cs
+++ Interview/Interview.UI/Services/Graph/IUsers.cs
@@ -6,9 +6,9 @@ namespace Interview.UI.Services.Graph
     public interface IUsers
     {
 
-        Task<EntraUser> GetUserInfo(string userPrincipalName, string token);
+        Task<EntraUser> GetUserInfoAsync(string userPrincipalName, string token);
 
-        Task<SearchUsersResponse> SearchInternalUsers(string query, string token);
+        Task<SearchUsersResponse> SearchInternalUsersAsync(string query, string token);
 
     }
 
