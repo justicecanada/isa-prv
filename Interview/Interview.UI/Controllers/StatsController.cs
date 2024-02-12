65ba48b42f1b0ade12ab755d41969b9284ca707f
Stewart Partington <Stewart.Partington@justice.gc.ca>
2024-02-12 12:14:46 -0500

    Initial Checkin

--- Interview/Interview.UI/Controllers/StatsController.cs
+++ Interview/Interview.UI/Controllers/StatsController.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using GoC.WebTemplate.Components.Core.Services;
 using Interview.UI.Models.AppSettings;
+using Interview.UI.Models.Stats;
 using Interview.UI.Services.DAL;
+using Interview.UI.Services.State;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Options;
@@ -15,15 +17,18 @@ namespace Interview.UI.Controllers
         #region Declarations
 
         private readonly IMapper _mapper;
+        private readonly IState _state;
 
         #endregion
 
         #region Constructors
 
-        public StatsController(IModelAccessor modelAccessor, DalSql dal, IMapper mapper, IStringLocalizer<BaseController> baseLocalizer) 
+        public StatsController(IModelAccessor modelAccessor, DalSql dal, IMapper mapper, IStringLocalizer<BaseController> baseLocalizer, 
+            IState state) 
             : base(modelAccessor, dal, baseLocalizer)
         {
             _mapper = mapper;
+            _state = state;
         }
 
         #endregion
@@ -31,10 +36,35 @@ namespace Interview.UI.Controllers
         #region Public Index Methods
 
         [HttpGet]
-        public IActionResult Index()
+        public async Task<IActionResult> Index(string processIdToFilter = null)
         {
 
-            return View();
+            VmIndex result = new VmIndex();
+            Guid processId = processIdToFilter == null ? (Guid)_state.ProcessId : new Guid(processIdToFilter);
+
+            result.ProcessId = processId;
+            await SetIndexViewBag();
+
+            return View(result);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SwitchProcess(string processId)
+        {
+
+            return RedirectToAction("Index", "Stats", new { processIdToFilter = processId } ) ;
+
+        }
+
+        private async Task SetIndexViewBag()
+        {
+
+            List<Entities.Process> processes = await GetProcessesForLoggedInUser();
+            ViewBag.Processes = processes;
+
+            ViewBag.ProcessId = (Guid)_state.ProcessId;
 
         }
 
