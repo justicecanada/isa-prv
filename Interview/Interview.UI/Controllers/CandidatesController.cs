11865c8f6c778f988008e6fa2b9a80941372aa49
Stewart Partington <Stewart.Partington@justice.gc.ca>
2024-02-22 13:08:55 -0500

    Interim checkin

--- Interview/Interview.UI/Controllers/CandidatesController.cs
+++ Interview/Interview.UI/Controllers/CandidatesController.cs
@@ -3,9 +3,12 @@ using GoC.WebTemplate.Components.Core.Services;
 using Interview.Entities;
 using Interview.UI.Models;
 using Interview.UI.Services.DAL;
+using Interview.UI.Services.State;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Localization;
+using System.Diagnostics;
+using Process = Interview.Entities.Process;
 
 namespace Interview.UI.Controllers
 {
@@ -16,15 +19,18 @@ namespace Interview.UI.Controllers
         #region Declarations
 
         private readonly IMapper _mapper;
+        private readonly IState _state;
 
         #endregion
 
         #region Constructors
 
-        public CandidatesController(IModelAccessor modelAccessor, DalSql dal, IStringLocalizer<BaseController> baseLocalizer, IMapper mapper)
+        public CandidatesController(IModelAccessor modelAccessor, DalSql dal, IStringLocalizer<BaseController> baseLocalizer, IMapper mapper, 
+            IState state)
             : base(modelAccessor, dal, baseLocalizer)
         {
             _mapper = mapper;
+            _state = state;
         }
 
         #endregion
@@ -58,26 +64,41 @@ namespace Interview.UI.Controllers
 
         #region External Methods
 
+        [HttpGet]
         public async Task<IActionResult> External(Guid processId, Guid externalCandidateId)
         {
 
+            _state.ProcessId = processId;
             await SetExternalViewBag(processId, externalCandidateId);
 
             return View();
 
         }
 
+        [HttpGet]
+        public async Task<IActionResult> SelectExternalInterview(Guid interviewId, Guid externalCandidateId)
+        {
+
+            await SetExternalViewBag((Guid)_state.ProcessId, externalCandidateId);
+
+            return View("External");
+
+        }
+
         private async Task SetExternalViewBag(Guid processId, Guid externalCandidateId)
         {
 
-            Process process = await _dal.GetEntity<Process>(processId) as Process;
+            Process process = await _dal.GetEntity<Process>(processId, true) as Process;
             List<Interview.Entities.Interview> interviews = await _dal.GetInterViewsByProcessId(processId);
-            List<VmInterview> vmInterviews = _mapper.Map<List<VmInterview>>(interviews);
+            List<Entities.Interview> availableInterviews = interviews.Where(x => x.Status == InterviewStates.AvailableForCandidate).ToList();
+            List<VmInterview> vmAvailableInterviews = _mapper.Map<List<VmInterview>>(availableInterviews);
+            List<VmRoleUser> vmRoleUsers = _mapper.Map<List<VmRoleUser>>(process.RoleUsers);
 
             ViewBag.ProccessStartDate = process.StartDate;
             ViewBag.ProccessEndDate = process.EndDate;
-            ViewBag.VmInterviews = vmInterviews;
+            ViewBag.VmInterviews = vmAvailableInterviews;
             ViewBag.ExternalCandidateId = externalCandidateId;
+            ViewBag.VmRoleUsers = vmRoleUsers;
 
         }
 
